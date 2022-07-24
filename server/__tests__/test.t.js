08df3dfa7fadd481cbda6e19728eb780fc6a327c
conner1reimers <66189525+conner1reimers@users.noreply.github.com>
2022-07-24 16:04:29 -0500

    .

--- server/__tests__/test.t.js
+++ server/__tests__/test.t.js
@@ -70,7 +70,6 @@ describe('Test Functions', function () {
                 expect(response.statusCode).toBe(200);
                 expect(response.body).toEqual(
                     expect.objectContaining({
-
                         hasPreviousPurchase: expect.any(Boolean),
                         uid: expect.any(Number),
                         userInfo: expect.any(Object)
@@ -80,13 +79,6 @@ describe('Test Functions', function () {
     })
 
 
-                  
-
-
-                })
-            )
-            })
-        })
     test('responds to api/fuel/submitquote', () => {
 
         return request(app)
